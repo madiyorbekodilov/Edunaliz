372975ee563b14fe35a94a45cd9b88c5bc1fea23
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-09-19 20:28:00 +0500

    Add Repository

--- src/Edunaliz.DataAccess/IRepositories/IRepository.cs
+++ src/Edunaliz.DataAccess/IRepositories/IRepository.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Edunaliz.Domain.Commons;
+using System.Linq.Expressions;
 
-namespace Edunaliz.DataAccess.IRepositories
+namespace Edunaliz.DataAccess.IRepositories;
+public interface IRepository<TEntity> where TEntity : Auditable
 {
-    internal class IRepository
-    {
-    }
+    Task AddAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    void Destroy(TEntity entity);
+    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression, string[] includes = null);
+    IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> expression = null, bool isNoTracked = true, string[] includes = null);
+    Task SaveAsync();
 }
